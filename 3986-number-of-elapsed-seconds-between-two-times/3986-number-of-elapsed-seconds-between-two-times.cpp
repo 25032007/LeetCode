58185adb046d5b75e77f6699eaa2df41d50a0c00
Anjali Mulchandani <anjalimulchandani2007@gmail.com>
2026-07-14 00:04:54 +0530

    Time: 0 ms (100%), Space: 9.1 MB (71.43%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.cpp
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.cpp
@@ -1,18 +1,13 @@
 class Solution {
+    int time(string& s){
+        int h = (s[0]-'0')*10 + (s[1]-'0');
+        int m = (s[3]-'0')*10 + (s[4]-'0');
+        int ss = (s[6]-'0')*10 + (s[7]-'0');
+
+        return (h*3600) + (m*60) + ss;
+    }
 public:
     int secondsBetweenTimes(string startTime, string endTime) {
-        int startHH = stoi(startTime.substr(0, 2)); //substr(strating index, length)
-        int startMM = stoi(startTime.substr(3, 2));
-        int startSS = stoi(startTime.substr(6, 2));
-
-        int SS1 = (startHH *3600) + (startMM*60) + startSS;
-
-        int endHH = stoi(endTime.substr(0,2));
-        int endMM = stoi(endTime.substr(3,2));
-        int endSS = stoi(endTime.substr(6,2));
-
-        int SS2 = (endHH*3600) + (endMM*60) + endSS;
-
-        return SS2 - SS1;
+        return time(endTime) - time(startTime);
     }
 };
