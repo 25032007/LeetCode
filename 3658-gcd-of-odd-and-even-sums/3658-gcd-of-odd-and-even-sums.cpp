05f3370b30fdbb4a011c1c35aed2ec7b1e9bc350
Anjali Mulchandani <anjalimulchandani2007@gmail.com>
2026-07-15 19:46:00 +0530

    Time: 0 ms (100%), Space: 8.6 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        return n;
+//         eg : n = 4
+//         sumOdd = 1, 3, 5, 7 = 16 => 4 ^ 2
+//         sumEven = 2, 4, 6, 8 = 20 => 4 * (4 + 1)
+
+//         sumOdd = n ^ 2
+//         sumEven = n * (n + 1)
+
+//         gcd(n *n , n* (n + 1))
+//         n * gcd(n, n + 1)
+//         Ab koi bhi do lagatar numbers (consecutive integers) jaise 4 aur 5, ya 9 aur 10 lijiye,
+    //    unka GCD hamesha 1 hota hai kyunki unke beech kuch bhi common nahi hota.
+//         Toh GCD(n, n + 1) = 1 ho gaya.
+
+// n * 1 = 1
+
+    return n;
+
     }
 };
