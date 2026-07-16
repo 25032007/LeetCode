afde5cb4364ef69c06daf5d3560cc83a3412c6ad
Anjali Mulchandani <anjalimulchandani2007@gmail.com>
2026-07-16 22:41:47 +0530

    Time: 0 ms (100%), Space: 7.9 MB (22.53%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        int dup = 0;
+        while(num > 0){
+            dup += num % 10;
+            cout << dup;
+            if(dup == 0) return false;
+            num/=10;
+        }
+        return true;
+    }
+};
