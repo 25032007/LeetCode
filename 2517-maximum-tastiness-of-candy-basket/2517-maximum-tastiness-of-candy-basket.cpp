0e453ea56ae3d0ebf6aea69642ab885d1d17f9a7
Anjali Mulchandani <anjalimulchandani2007@gmail.com>
2026-07-16 13:38:32 +0530

    Time: 46 ms (66.51%), Space: 52.1 MB (99.88%) - LeetHub

---
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    //Can I choose k element or not 
+    int check(vector<int>& price, int k, int mid){
+        int count = 1, last = price[0];
+        int n = price.size();
+        for(int i=0; i<n; i++){
+            if(price[i]- last >= mid){
+                last = price[i];
+                count++;
+            }
+        }
+        return count >= k;
+    }
+public:
+    int maximumTastiness(vector<int>& price, int k) {
+        int n = price.size();
+        sort(price.begin(), price.end());
+        int low = 0, high = price[n-1], ans = 0;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(check(price, k, mid)){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
+
+
