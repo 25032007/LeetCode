a6d422ce7627488849bb795499a739df27addffa
Anjali Mulchandani <anjalimulchandani2007@gmail.com>
2026-07-17 09:09:13 +0530

    Time: 0 ms (100%), Space: 11.9 MB (24.83%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -3,30 +3,22 @@ public:
     int longestValidParentheses(string s) {
         int n = s.size();
         stack<int> st;
-        vector<int> visited(n, 0);
+        int maxi = 0;
 
+        st.push(-1);
         for(int i=0; i<n; i++){
             if(s[i] == '('){
                 st.push(i);
             }
             else{
-                if(!st.empty()){
-                    visited[i] = 1;
-                    visited[st.top()] = 1;
-                    st.pop();
+                st.pop();
+                if(st.empty()){
+                    st.push(i);
+                }
+                else{
+                    maxi = max(maxi, i - st.top());
                 }
             }
-        }
-
-        int count = 0, maxi = 0;
-        for(int x: visited){
-            if(x == 1){
-                count++;
-            }
-            else{
-                count = 0;
-            }
-            maxi = max(maxi, count);
         }
         return maxi;
     }
